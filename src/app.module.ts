c8f76fe7b1e7518fca80b61604d5ff894ad29615
atul <atulverma2861@gmail.com>
2026-02-07 13:12:00 +0530

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from './config';
 import { HealthModule } from './modules/health';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [ConfigModule, HealthModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
